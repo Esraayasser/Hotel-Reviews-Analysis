bd841a0f6a93e86b03b2a6946adbfddd1ae99ee8
mennaawad <mennamawad16@gmail.com>
2019-11-28 20:15:04 +0200

    Linear Regression with average score

---
+++ LinearRegression.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn import linear_model
+from sklearn import metrics
+
+
+def Linear_Regression_averageScore(x_train, x_test, y_train, y_test):
+    model = linear_model.LinearRegression()
+
+    x_train = x_train[:, 1]
+
+    x_train = np.expand_dims(x_train, axis=1)
+   # y_train = np.expand_dims(y_train, axis=1)
+
+    model.fit(x_train, y_train)
+
+    prediction = model.predict(x_train)
+    train_error = metrics.mean_squared_error(y_train, prediction)
+    print('Mean Square Error of Linear Regression with Average Score feature on train set: ', train_error)
+
+    x_test = x_test[:, 1]
+
+    x_test = np.expand_dims(x_test, axis=1)
+    #y_test = np.expand_dims(y_test, axis=1)
+
+    prediction_test = model.predict(x_test)
+    test_error = metrics.mean_squared_error(y_test, prediction_test)
+    print('Mean Square Error of Linear Regression with Average Score feature on test set: ', test_error)
+
+    return train_error, test_error
+
+
--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from Preprocessing import *
 from PolynomialRegression import *
+from LinearRegression import *
 
 x_train, x_test, y_train, y_test = preprocessing()
-poly_error = polynomial_regression(x_train, x_test, y_train, y_test)
+poly_error = polynomial_regression_all(x_train, x_test, y_train, y_test)
+train_error , test_error = Linear_Regression_averageScore(x_train, x_test, y_train, y_test)
+lin1, lin2 = Linear_Regression_averageScore(x_train, x_test, y_train, y_test)
