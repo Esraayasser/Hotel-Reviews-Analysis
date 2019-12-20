9be67886ddaa0062244c1815e59730e8b9d1e61f
Mirna <MirnaMuhammad98@gmail.com>
2019-12-20 17:46:33 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+from Preprocessing import *
+from PolynomialRegression import *
+from LinearRegression import *
+from MultipleLinearRegression import *
 import SVM
 import Testing
 
@@ -28,3 +32,24 @@ models_accuracies, models_testing_time = Testing.model_testing(models, X_test, y
 for i in range(len(models)):
     print("Model name is {} - accuracy = {} and model time = {}".
           format(models_names[i], models_accuracies*[i], models_testing_time[i]))
+
+# Regression Milestone1
+'''
+x_train, x_test, y_train, y_test, X, Y = preprocessing()
+
+linearAS_train_error, linearAS_test_error, modelAS = Linear_Regression_averageScore(x_train, x_test, y_train, y_test)
+PlotLinearModel(X[:, 1], Y, modelAS, 'Average Score', 'Reviewer Score')
+
+linearPRC_train_Error, LinearPRC_test_Error, modelPRC = Linear_Regression_PositiveReviewCount(x_train, x_test, y_train, y_test)
+PlotLinearModel(X[:, 6], Y, modelPRC, 'Positive Review Word Count', 'Reviewer Score')
+
+linearNRC_train_error, linearNRC_test_error, modelNRC = LinearRegressionNegativeReviewCount(x_train, x_test, y_train, y_test)
+PlotLinearModel(X[:, 4], Y, modelNRC, 'Negative Review Word Count', 'Reviewer Score')
+
+#multipleLR1_train_Error, multipleLR1_test_Error = Multiple_Linear_Regression1(x_train, x_test, y_train, y_test)
+multipleLR1_test_Error = Multiple_Linear_Regression1(x_train, x_test, y_train, y_test)
+multipleLR_PN_error = MultipleLinearRegressionPositiveNegative(x_train, x_test, y_train, y_test)
+
+poly1_error_train, poly1_error_test = polynomial_regression_all(x_train, x_test, y_train, y_test)
+poly2_error_train, poly2_error_test = polynomial_regression_top_three(x_train, x_test, y_train, y_test)
+'''
