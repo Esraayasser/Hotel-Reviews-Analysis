1eb2ee35f50c99e4d6fe5d08547d2f032a67e004
emanalaa <eman3laa@outlook.com>
2019-12-22 09:30:00 +0200

    update testing

--- Testing.py
+++ Testing.py
@@ -2,8 +2,11 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 from sklearn import tree
+from sklearn import linear_model
+from sklearn import metrics
 import time
 import numpy as np
+
 
 def model_testing(models, x_test, y_test):
     models_accuracies = []
@@ -18,3 +21,24 @@ def model_testing(models, x_test, y_test):
         models_accuracies.append(accuracy)
 
     return models_accuracies, models_testing_time
+
+
+def regression_testing(models, x_test, y_test):
+    models_mean_square_error = []
+    models_testing_time = []
+    models_r2_score = []
+
+    for i in range(len(models)):
+        start_time = time.time()
+
+        prediction_test = models[i].predict(x_test)
+        test_error = metrics.mean_squared_error(y_test, prediction_test)
+        r2_score = metrics.r2_score(y_test, prediction_test)
+
+        end_time = time.time()
+
+        models_r2_score.append(r2_score)
+        models_mean_square_error.append(test_error)
+        models_testing_time.append(abs(start_time - end_time))
+
+        return models_testing_time, models_r2_score, models_mean_square_error
